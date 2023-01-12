e02ffee9b970c637c605ff57f02a3f9e65f77071
Butajoao <90648314+Butajoao@users.noreply.github.com>
2023-01-12 16:14:38 -0300

    Inicial-javascript

---
+++ type-number.js
@@ -0,0 +1,24 @@
+
+
+const meuNumero = 3 ;
+
+const primeiroNumero = 1 ; 
+const segundoNumero = 2 ;
+
+const operacaoMatematica = primeiroNumero + segundoNumero ;
+const bitelo21 = 'Tmj Ronaldo'
+
+
+console.log(operacaoMatematica) ;
+console.log(bitelo21) ;
+
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
